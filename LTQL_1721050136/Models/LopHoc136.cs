dead35e2d221a968b06f54efb2c77b1424f29756
min <min@DESKTOP-T2NAVI1>
2021-12-21 13:04:11 +0700

    Create_Table_LopHoc_LNG136

---
+++ LTQL_1721050136/Models/LopHoc136.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LTQL_1721050136.Models
+{
+    [Table("LopHoc136")]
+    public class LopHoc136
+    {
+        [Key]
+        [Display(Name ="Mã Lớp")]
+        public int MaLop { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Tên Lớp")]
+        public string TenLop { get; set; }
+
+    }
+}
